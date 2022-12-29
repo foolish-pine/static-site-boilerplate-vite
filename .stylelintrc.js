b952bd8068393d55c2c5dfb480460161197ef9d7
foolish-pine <matsuda.takuya41@gmail.com>
2022-12-29 14:36:33 +0900

    chore: コメント追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
 	extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
 	rules: {
-		indentation: "tab",
+		indentation: "tab", // Editor Configの設定と整合性を取るため
 		"keyframes-name-pattern": null,
 		"scss/at-function-pattern": null,
 		"scss/at-mixin-pattern": null,
 		"scss/dollar-variable-pattern": null,
 		"scss/percent-placeholder-pattern": null,
-		"scss/selector-no-union-class-name": true,
+		"scss/selector-no-union-class-name": true, // class名の検索性の低下を防ぐため
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
 	},

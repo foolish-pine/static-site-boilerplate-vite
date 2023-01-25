c1505b0a42de251e46d6ad75ad63cfcfb83d1502
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-25 22:25:56 +0900

    chore: eslintrcの拡張子をjsに変更

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	env: {
+		es2022: true,
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: "latest",
+	},
+	extends: ["eslint:recommended"],
+	plugins: ["import", "unused-imports"],
+	rules: {
+		"import/order": [2, { alphabetize: { order: "asc" } }],
+		"unused-imports/no-unused-imports": "error",
+	},
+};

7daaa954136dd528ad41f8302d1832a90b8c20b5
foolish-pine <matsuda.takuya41@gmail.com>
2023-07-16 15:21:13 +0900

    chore: remove comment

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
 	extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
 	rules: {
-		indentation: "tab", // Editor Configの設定と整合性を取るため
+		indentation: "tab",
 		"keyframes-name-pattern": null,
 		"scss/at-function-pattern": null,
 		"scss/at-mixin-pattern": null,
 		"scss/dollar-variable-pattern": null,
 		"scss/percent-placeholder-pattern": null,
-		"scss/selector-no-union-class-name": true, // class名の検索性の低下を防ぐため
+		"scss/selector-no-union-class-name": true,
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
 	},
--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,9 @@ import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 import viteImagemin from "vite-plugin-imagemin";
 
-// 各ページのメタデータを読み込む
-const metadata = JSON.parse(
-	fs.readFileSync(resolve(__dirname, "./src/metadata.json"))
-);
-
-// HTMLの複数出力を自動化する
-//./src配下のファイル一式を取得
+const metadata = JSON.parse(fs.readFileSync(resolve(__dirname, "./src/metadata.json")));
 const fileNameList = fs.readdirSync(resolve(__dirname, "./src/"));
-
-//htmlファイルのみ抽出
 const htmlFileList = fileNameList.filter((file) => /.html$/.test(file));
-
-//build.rollupOptions.inputに渡すオブジェクトを生成
 const inputFiles = {};
 for (let i = 0; i < htmlFileList.length; i++) {
 	const file = htmlFileList[i];
@@ -24,17 +14,15 @@ for (let i = 0; i < htmlFileList.length; i++) {
 }
 export default defineConfig({
 	server: {
-		host: true, //IPアドレスを有効化
+		host: true,
 	},
-	root: "./src", //開発ディレクトリ設定
+	root: "./src",
 	build: {
-		outDir: "../dist", //出力場所の指定
+		outDir: "../dist",
 		rollupOptions: {
-			//ファイル出力設定
 			output: {
 				assetFileNames: (assetInfo) => {
 					let extType = assetInfo.name.split(".")[1];
-					//Webフォントファイルの振り分け
 					if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
 						extType = "fonts";
 					}
@@ -51,15 +39,12 @@ export default defineConfig({
 	},
 	plugins: [
 		handlebars({
-			//コンポーネントの格納ディレクトリを指定
 			partialDirectory: resolve(__dirname, "./src/html-components"),
-			//各ページ情報の読み込み
 			context(pagePath) {
 				return metadata[pagePath];
 			},
 		}),
 		viteImagemin({
-			// 画像圧縮の設定
 			gifsicle: {
 				optimizationLevel: 3,
 			},

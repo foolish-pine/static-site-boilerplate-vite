88c3e2f70a5c2e289d13d656da4e967f2aee9ec4
foolish-pine <59694183+foolish-pine@users.noreply.github.com>
2024-06-21 11:38:14 +0900

    chore: simplify vite settings (#114)

--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,26 @@ import fs from "fs";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
-import viteImagemin from "vite-plugin-imagemin";
 
 const metadata = JSON.parse(
   fs.readFileSync(resolve(__dirname, "./src/metadata.json")),
 );
-const fileNameList = fs.readdirSync(resolve(__dirname, "./src/"));
-const htmlFileList = fileNameList.filter((file) => /.html$/.test(file));
-const inputFiles = {};
-for (let i = 0; i < htmlFileList.length; i++) {
-  const file = htmlFileList[i];
-  inputFiles[file.slice(0, -5)] = resolve(__dirname, "./src/" + file);
+const fileNames = fs.readdirSync(resolve(__dirname, "./src/"));
+const htmlFiles = fileNames.filter((file) => /.html$/.test(file));
+const input = {};
+for (let i = 0; i < htmlFiles.length; i++) {
+  const file = htmlFiles[i];
+  input[file.slice(0, -5)] = resolve(__dirname, "./src/" + file);
 }
 export default defineConfig({
   server: {
     host: true,
+    open: true,
   },
   root: "./src",
   build: {
-    outDir: "../dist",
     rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".")[1];
-          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-            extType = "fonts";
-          }
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name][extname]`;
-        },
-        chunkFileNames: "assets/js/[name].js",
-        entryFileNames: "assets/js/[name].js",
-      },
-      input: inputFiles,
+      input,
     },
   },
   plugins: [
@@ -44,26 +29,6 @@ export default defineConfig({
       partialDirectory: resolve(__dirname, "./src/html-components"),
       context(pagePath) {
         return metadata[pagePath];
-      },
-    }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 3,
-      },
-      mozjpeg: {
-        quality: 65,
-        progressive: true,
-      },
-      pngquant: {
-        quality: [0.65, 0.8],
-        speed: 1,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: "removeViewBox",
-          },
-        ],
       },
     }),
   ],
